89cac0696650d8e89523c58406c32245fff7e461
Dev Nand D <145089850+DevNand2003@users.noreply.github.com>
2025-02-21 12:03:24 +0530

    Update settings.py

--- billing_system/settings.py
+++ billing_system/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Or your email host
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'devnand2003@gmail.com'  # Your email
-EMAIL_HOST_PASSWORD = 'hlzo vogg tgoq gqhz'
+EMAIL_HOST_USER = '1234@gmail.com'  # Your email
+EMAIL_HOST_PASSWORD = 'password'
